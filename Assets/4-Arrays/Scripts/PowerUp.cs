b747d895e71a34f401b5b5f9796cb035e7208b23
JoelHughesy <joeleeny92@gmail.com>
2017-09-22 15:56:18 +1000

    Made Arrays Game

---
+++ Assets/4-Arrays/Scripts/PowerUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arrays
+{
+
+    public class PowerUp : MonoBehaviour
+    {
+        public float speedIncrease = 5f;
+        public float rotationSpeed = 360f;
+        
+        void OnTriggerEnter(Collider other)
+        {
+            Player p = other.GetComponent<Player>();
+            if(p != null)
+            {
+                // Increase Player's speed
+                p.movementSpeed += speedIncrease;
+                // 
+
+                //Destroy self
+                Destroy(gameObject);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
+
+        }
+    }
+}
